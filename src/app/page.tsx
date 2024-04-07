fd600d03d263fbb4fd824417e902721f35da02bd
hungtq1016 <hungtq1016@gmail.com>
2024-04-07 15:22:06 +0700

    update new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
+/* eslint-disable @next/next/no-img-element */
+
 import Link from 'next/link'
 
 export default function Page() {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-yellow-600 min-h-screen">
       <main>
         {/* Hero section */}
         <div className="relative isolate overflow-hidden">
@@ -36,7 +37,7 @@ export default function Page() {
             aria-hidden="true"
           >
             <div
-              className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+              className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-sky-900 opacity-20"
               style={{
                 clipPath:
                   'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
@@ -45,45 +46,29 @@ export default function Page() {
           </div>
           <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
             <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-              <img
-                className="h-11"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              />
-              <div className="mt-24 sm:mt-32 lg:mt-16">
-                <a href="#" className="inline-flex space-x-6">
-                  <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                    Latest updates
-                  </span>
-                  <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                    <span>Just shipped v1.0</span>
-                    <ChevronRightIcon className="h-5 w-5 text-gray-500" aria-hidden="true" />
-                  </span>
-                </a>
-              </div>
+
               <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Deploy to the cloud with confidence
+              &quot;Think It, Ink It.&quot;
               </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+              <p className="mt-6 text-lg leading-8 text-gray-100">
+              Transform fleeting thoughts into lasting achievements. Capture, organize, and elevate every idea. Your pocket-sized catalyst for creativity and productivity..
               </p>
               <div className="mt-10 flex items-center gap-x-6">
                 <Link
                   href="/api/auth/signin"
-                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                  className="rounded-md bg-sky-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                 >
-                  Get started
+                  Sign In
                 </Link>
                 <Link href="/note/me" className="text-sm font-semibold leading-6 text-white">
-                  Live demo <span aria-hidden="true">→</span>
+                  Open App <span aria-hidden="true">→</span>
                 </Link>
               </div>
             </div>
             <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
               <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                 <img
-                  src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                  src="/section.png"
                   alt="App screenshot"
                   width={2432}
                   height={1442}
